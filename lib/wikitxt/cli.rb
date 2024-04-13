7627168f0c7841c89da91115ebfc472f1981625c
Moeki Kawakami <moeki.kawakami@icloud.com>
2024-04-13 14:11:48 +0900

    add automatic index page

--- lib/wikitxt/cli.rb
+++ lib/wikitxt/cli.rb
@@ -11,14 +11,19 @@ module Wikitxt
     desc "server [PATH]", "start wiki site"
     def server(path)
       files = Dir.glob(File.join(path, "*.txt"))
+      pages = []
       files.each do |file|
         text = File.read(file)
         body = Wikitxt.to_html(text)
         title = File.basename(file, ".txt")
+        pages << title
         html = ERB.new(File.read(File.join(__dir__, "../", "html", "layout.html.erb"))).result_with_hash({ body: body, title: title })
         Dir.mkdir("dist") unless Dir.exist?("dist")
         File.write(File.join("dist", "#{title}.html"), html)
       end
+      index = ERB.new(File.read(File.join(__dir__, "../", "html", "index.html.erb"))).result_with_hash({ pages: pages })
+      html = ERB.new(File.read(File.join(__dir__, "../", "html", "layout.html.erb"))).result_with_hash({ body: index, title: "Index" })
+      File.write(File.join("dist", "index.html"), html)
       images = Dir.glob(File.join(path, "*.{png,jpg,jpeg}"))
       FileUtils.cp(images, "dist")
       server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => "dist"
